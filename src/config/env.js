2f38717de026faca704aee4e7b48c60eb3cd132e
kupets <kupets@evergreens.in.ua>
2025-04-27 00:47:12 +0300

    feat: add S3 env

--- src/config/env.js
+++ src/config/env.js
@@ -4,4 +4,10 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     JWT_SECRET: process.env.JWT_SECRET || 'supersecret',
     DATABASE_URL: process.env.DATABASE_URL,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    AWS_ENDPOINT: process.env.AWS_ENDPOINT,
+    AWS_FORCE_PATH_STYLE: process.env.AWS_FORCE_PATH_STYLE,
 };
