9b466075722fc7705717af73b4195b112a62dfcd
kupets <kupets@evergreens.in.ua>
2025-04-26 23:37:50 +0300

    BREAKING CHANGES: add confirm password in cotrolles

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -3,16 +3,19 @@ const { hashPassword, comparePassword } = require('../../common/utils/password')
 const { generateToken } = require('../../common/utils/jwt');
 const ApiError = require('../../common/exceptions/ApiError');
 
-const register = async ({ login, email, password }) => {
+const register = async ({ email, password, confirmPassword }) => {
     const existUser = await User.findOne({ where: { email } });
     if (existUser) {
         throw new ApiError(400, 'User with this email already exists');
     }
 
+    if (password !== confirmPassword) {
+        throw new ApiError(400, 'Passwords do not match');
+    }
+
     const hashedPassword = await hashPassword(password);
 
     const user = await User.create({
-        login,
         email,
         password: hashedPassword,
     });
