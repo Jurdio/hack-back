d86f147898cb2a17fbd5c3548e2efcf28a566a7c
kupets <kupets@evergreens.in.ua>
2025-04-27 00:49:53 +0300

    feat: create upoad midlware

---
+++ src/common/middlewares/upload.middleware.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+
+// Зберігання в пам'яті
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only images are allowed!'), false);
+    }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
