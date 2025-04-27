3741e99ff5f990e1f10ddef1e9ac545483973002
kupets <kupets@evergreens.in.ua>
2025-04-27 03:50:57 +0300

    BREAKING CHANGES: change ai model on gemini

--- src/common/services/ai.service.js
+++ src/common/services/ai.service.js
@@ -1,21 +1,15 @@
-const { openai } = require('../clients/openai');
+const { genAI } = require('../clients/gemini');
 
 async function generateSummary(text) {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+
     const prompt = `Стисло і чітко сформулюй основну ідею наступного тексту за 500 символів:\n\n${text}`;
 
-    const response = await openai.createChatCompletion({
-        model: 'gpt-4', // або gpt-3.5-turbo
-        messages: [
-            {
-                role: 'user',
-                content: prompt,
-            },
-        ],
-        temperature: 0.5,
-        max_tokens: 300,
-    });
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const summary = response.text();
 
-    return response.data.choices[0].message.content.trim();
+    return summary.trim();
 }
 
 module.exports = {
